93ec9c7fcfb81a08e12e4ffc74a75513d80b170d
chaerrrish <95009493+Chaerrrish@users.noreply.github.com>
2024-01-05 22:37:51 +0900

    Title: 살 수 있는 물건 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240105/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -0,0 +1,14 @@
+book = 3000
+mask = 1000
+pen = 500
+
+n = int(input())
+
+if n >= book :
+    print("book")
+elif n >= mask :
+    print("mask")
+elif n >= pen :
+    print("pen")
+else:
+    print("no")
